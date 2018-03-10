f3050d82ca0467d1479d443d9533aa8245838629
JD Taft <twoplustwois5@gmail.com>
2018-03-10 01:58:12 -0600

    Design Recipe List

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test', 'Just a Test', 'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_960_720.jpg'),
+    new Recipe('Test 2', 'Just a Test 2', 'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_960_720.jpg')
+  ];
 
   constructor() { }
 
